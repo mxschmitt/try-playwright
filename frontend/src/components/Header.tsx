e4be8caa8353a0e7803475667ca9506e5d3ab32b
Max Schmitt <max@schmitt.mx>
2020-03-02 23:26:10 +0100

    feat: make header link to Try Playwright clickable

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     <Navbar appearance="inverse">
       <Navbar.Header>
         <Nav>
-          <Nav.Item><strong>Try Playwright</strong></Nav.Item>
+          <Nav.Item href="/"><strong>Try Playwright</strong></Nav.Item>
         </Nav>
       </Navbar.Header>
       <Navbar.Body>
