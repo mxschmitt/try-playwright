87b33ee4019d13300e6d44aff5596336ac965440
Max Schmitt <max@schmitt.mx>
2020-03-08 23:02:10 +0100

    fix: cleanup

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,5 +1,4 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const webpack = require("webpack")
 
 module.exports = function override(config, env) {
   config.plugins.push(new MonacoWebpackPlugin({
