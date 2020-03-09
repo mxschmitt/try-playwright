1cfb4409fc979e0bafcc7f4cd85a7753f938d492
Max Schmitt <max@schmitt.mx>
2020-03-09 16:07:43 +0100

    feat: cleanup editor (#34)
    * feat: cleanup editor
    
    * Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { Row, Col, Grid, IconButton, Icon, Loader, Panel, Dropdown, Notification, Message } from 'rsuite'
+import { Col, Grid, IconButton, Icon, Loader, Panel, Dropdown, Notification, Message } from 'rsuite'
 import MonacoEditor from 'react-monaco-editor';
 import * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 
@@ -13,6 +13,18 @@ import useDarkMode from './hooks/useDarkMode';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import staticTypes from '!!raw-loader!./types.txt';
+
+const MONACO_OPTIONS: monacoEditor.editor.IEditorConstructionOptions = {
+  minimap: {
+    enabled: false
+  },
+  scrollBeyondLastLine: false,
+  hideCursorInOverviewRuler: true,
+  overviewRulerLanes: 0,
+  scrollbar: {
+    vertical: "hidden"
+  }
+}
 
 const App: React.FunctionComponent = () => {
   const [darkMode] = useDarkMode()
@@ -70,7 +82,7 @@ const App: React.FunctionComponent = () => {
         }
       }
     });
-    monaco.languages.typescript.typescriptDefaults.addExtraLib(staticTypes)
+    monaco.languages.typescript.javascriptDefaults.addExtraLib(staticTypes)
   }
   const RunButton: React.FunctionComponent = () => (
     <IconButton onClick={handleExecution} style={{ float: "right" }} icon={<Icon icon="play" />}>
@@ -89,64 +101,52 @@ const App: React.FunctionComponent = () => {
     <>
       <Header />
       <Grid fluid>
-        <Row>
-          <Col xs={24} md={12}>
-            {loading && <Loader center content="loading" backdrop style={{ zIndex: 10 }} />}
-            <Panel
-              bodyFill
-              header={
-                <>
-                  Examples{' '}
-                  <Dropdown title={example ? example.title : "Custom"} onSelect={handleSelectExample}>
-                    {Examples.map((example, idx) => <ExampleWrapper key={idx} example={example} index={idx} />)}
-                  </Dropdown>
-                  <RunButton />
-                </>
-              }
-            >
-              {example?.description && <Message description={example.description} style={{ margin: "0 20px 20px 20px", animation: "none" }} />}
-              <MonacoEditor
-                onChange={handleChangeCode}
-                language="typescript"
-                theme={darkMode ? "custom-dark" : "vs"}
-                value={code}
-                height={500}
-                options={{
-                  minimap: {
-                    enabled: false
-                  },
-                  scrollBeyondLastLine: false,
-                  hideCursorInOverviewRuler: true,
-                  overviewRulerLanes: 0,
-                  scrollbar: {
-                    vertical: "hidden"
-                  }
-                }}
-                editorDidMount={handleEditorDidMount}
-              />
-            </Panel>
-          </Col>
-          <Col xs={24} md={12}>
-            <Panel
-              bodyFill
-              header={
-                <>
-                  Output <ShareButton code={code} style={{ float: "right" }} />
-                </>
-              }
-            >
-              {resp && <>
-                {resp.logs.length > 0 && <h4>Logs</h4>}
-                <code>{resp.logs.map((entry, idx) => <React.Fragment key={idx}>
-                  {entry.args.join(" ")}
-                  <br />
-                </React.Fragment>)}</code>
-                {resp.files.length > 0 && <h4>Files</h4>}
-                {resp.files.map((file, idx) => <ResponseFile file={file} key={idx} />)}
-              </>}
-            </Panel>
-          </Col>
-        </Row>
+        <Col xs={24} md={12}>
+          {loading && <Loader center content="loading" backdrop style={{ zIndex: 10 }} />}
+          <Panel
+            bodyFill
+            header={
+              <>
+                Examples{' '}
+                <Dropdown title={example ? example.title : "Custom"} onSelect={handleSelectExample}>
+                  {Examples.map((example, idx) => <ExampleWrapper key={idx} example={example} index={idx} />)}
+                </Dropdown>
+                <RunButton />
+              </>
+            }
+          >
+            {example?.description && <Message description={example.description} style={{ margin: "0 20px 20px 20px", animation: "none" }} />}
+            <MonacoEditor
+              onChange={handleChangeCode}
+              language="javascript"
+              theme={darkMode ? "custom-dark" : "vs"}
+              value={code}
+              options={MONACO_OPTIONS}
+              editorDidMount={handleEditorDidMount}
+              height={500}
+            />
+          </Panel>
+        </Col>
+        <Col xs={24} md={12}>
+          <Panel
+            bodyFill
+            header={
+              <>
+                Output <ShareButton code={code} style={{ float: "right" }} />
+              </>
+            }
+          >
+            {resp && <>
+              {resp.logs.length > 0 && <h4>Logs</h4>}
+              <code>{resp.logs.map((entry, idx) => <React.Fragment key={idx}>
+                {entry.args.join(" ")}
+                <br />
+              </React.Fragment>)}</code>
+              {resp.files.length > 0 && <h4>Files</h4>}
+              {resp.files.map((file, idx) => <ResponseFile file={file} key={idx} />)}
+            </>}
+          </Panel>
+        </Col>
       </Grid >
     </>
   );
