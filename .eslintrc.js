64184be40558af6c931fcca0c01a3e9bb50b981c
Max Schmitt <max@schmitt.mx>
2020-10-22 12:28:05 +0200

    change test suite

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    "plugin:jest-playwright/recommended"
   ],
   rules: {
     "@typescript-eslint/ban-ts-comment": 0,
