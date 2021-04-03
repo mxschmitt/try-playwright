c7bbcad3d829b8cc8419c6c9d7f58c9cee69fc6e
Max Schmitt <max@schmitt.mx>
2021-04-03 13:39:12 +0200

    ok

---
+++ .github/workflows/determine_docker_image_name.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+IMAGE_ID="ghcr.io/$GITHUB_REPOSITORY/$1"
+
+if [[ "$GITHUB_EVENT_NAME" == 'pull_request' ]]; then
+    VERSION="pr-$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")"
+else
+    # Strip git ref prefix from version
+    VERSION=$(echo "$GITHU_REF" | sed -e 's,.*/\(.*\),\1,')
+fi
+
+# Strip "v" prefix from tag name
+[[ "$GITHUB_REF" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
+
+# Use Docker `latest` tag convention
+[ "$VERSION" == "master" ] && VERSION=latest
+
+echo "$IMAGE_ID:$VERSION"
