ff533d8d048e8d9dd445e81ecb9d32c16856a6a6
Max Schmitt <max@schmitt.mx>
2022-05-07 11:31:45 +0100

    chore: upgrade Playwright to 1.21

--- update_pw.sh
+++ update_pw.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+PLAYWRIGHT_VERSION="$1"
+
+if [ -z "$PLAYWRIGHT_VERSION" ]; then
+  echo "Usage: $0 <playwright version>"
+  exit 1
+fi
+
 function update_dependencies {
     cd $1
     npx npm-check-updates -u
@@ -35,7 +42,17 @@ function update_playwright_types {
     echo "$CUSTOM_SUFFIX" >> $TYPES_FILE
 }
 
+function update_pw_dockerfile_versions {
+    local languages=("csharp" "java" "javascript" "python")
+    for language in "${languages[@]}"
+    do
+        local docker_file="./worker-${language}/Dockerfile"
+        sed -i '' -e "s/ARG PLAYWRIGHT_VERSION=.*/ARG PLAYWRIGHT_VERSION=${PLAYWRIGHT_VERSION}/" $docker_file
+    done
+}
+
 
 update_dependencies frontend
 update_dependencies e2e
 update_playwright_types
+update_pw_dockerfile_versions
