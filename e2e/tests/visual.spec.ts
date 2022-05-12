90454fd8b104aab792267979ac921365cfc78752
Max Schmitt <max@schmitt.mx>
2022-05-12 16:21:06 +0100

    chore: rewrite end-to-end tests with web-first assertions (#246)

--- e2e/tests/visual.spec.ts
+++ e2e/tests/visual.spec.ts
@@ -1,108 +1,118 @@
-import { expect, test, Page } from '@playwright/test';
+import { expect, test as base, Page } from '@playwright/test';
 
-const executeExample = async (page: Page, nth: number): Promise<void> => {
-  await page.goto('/', { waitUntil: "networkidle" });
-  await page.click(`.rs-panel-group > .rs-panel:nth-child(${nth})`);
-  await Promise.all([
-    page.waitForResponse("**/service/control/run"),
-    page.click('text="Run"'),
-  ])
+class TryPlaywrightPage {
+  constructor(private readonly page: Page) { }
+  async executeExample(nth: number): Promise<void> {
+    await this.page.goto('/?l=javascript');
+    await this.page.locator(`.rs-panel-group > .rs-panel:nth-child(${nth})`).click();
+    await Promise.all([
+      this.page.waitForResponse("**/service/control/run"),
+      this.page.locator('text="Run"').click(),
+    ])
+  }
+  async getConsoleLines(): Promise<string[]> {
+    let consoleLines = this.page.locator(".rs-panel-body code")
+    await consoleLines.waitFor();
+    return await consoleLines.evaluate<string[], HTMLElement>((code) => code.innerText.split(/\n/).filter(Boolean))
+  }
+  images = this.page.locator('p[data-test-id="file"] > img');
+  pdfs = this.page.locator('p[data-test-id="file"] object');
+  videos = this.page.locator('p[data-test-id="file"] video');
+  fileNames = this.page.locator('p[data-test-id="file"] span.file-name')
 }
 
-const getImageCount = async (page: Page): Promise<number> => {
-  await page.waitForSelector('p[data-test-id="file"]')
-  return await page.$$eval('p[data-test-id="file"] > img', (images) => images.length)
-}
-const getVideoCount = async (page: Page): Promise<number> => {
-  await page.waitForSelector(".rs-panel-body video")
-  return await page.$$eval('p[data-test-id="file"] video', (videos) => videos.length)
-}
-const getFileNames = (page: Page): Promise<string[]> => page.$$eval<string[], HTMLSpanElement>('p[data-test-id="file"] span.file-name', (elements) => elements.map(el => el.innerText))
-const getConsoleLines = async (page: Page): Promise<string[]> => {
-  await page.waitForSelector(".rs-panel-body code")
-  return await page.$eval<string[], HTMLElement>(".rs-panel-body code", (code) => code.innerText.split(/\n/).filter(Boolean))
-}
+
+
+const test = base.extend<{ tpPage: TryPlaywrightPage }>({
+  tpPage: async ({ page }, using) => {
+    const tryPlaywrightPage = new TryPlaywrightPage(page);
+    await using(tryPlaywrightPage);
+  }
+});
 
 test.describe('Examples', () => {
-  test("1: should be able to make screenshots in all browsers", async ({ page }) => {
-    await executeExample(page, 1)
-    const imageCount = await getImageCount(page)
-    expect(imageCount).toBe(2)
-    const imageNames = await getFileNames(page)
-    expect(new Set(imageNames)).toEqual(new Set(["example-chromium.png", "example-webkit.png"]))
+  test("1: should be able to make screenshots in all browsers", async ({ tpPage }) => {
+    await tpPage.executeExample(1)
+    await expect(tpPage.images).toHaveCount(2)
+    await expect(tpPage.videos).toHaveCount(0)
+    await expect(tpPage.fileNames).toHaveText([
+      "example-chromium.png",
+      "example-webkit.png",
+    ])
   })
-  test("2: should be able to set the geolocation", async ({ page }) => {
-    await executeExample(page, 2)
-    const imageCount = await getImageCount(page)
-    expect(imageCount).toBe(1)
-    const imageNames = await getFileNames(page)
-    expect(imageNames).toEqual(["colosseum-iphone.png"])
+  test("2: should be able to set the geolocation", async ({ tpPage }) => {
+    await tpPage.executeExample(2)
+    await expect(tpPage.images).toHaveCount(1)
+    await expect(tpPage.videos).toHaveCount(0)
+    await expect(tpPage.fileNames).toHaveText([
+      "colosseum-iphone.png"
+    ])
   })
-  test("3: should be able to generate a PDF file", async ({ page }) => {
-    await executeExample(page, 3)
-    await page.waitForSelector(".rs-panel-body object")
-    const pdfCount = await page.$$eval('p[data-test-id="file"] object', (objects) => objects.length)
-    expect(pdfCount).toBe(1)
-    const imageNames = await getFileNames(page)
-    expect(imageNames).toEqual(["document.pdf"])
+  test("3: should be able to generate a PDF file", async ({ page, tpPage }) => {
+    await tpPage.executeExample(3)
+    await expect(tpPage.pdfs).toHaveCount(1)
+    await expect(tpPage.images).toHaveCount(0)
+    await expect(tpPage.videos).toHaveCount(0)
+    await expect(tpPage.fileNames).toHaveText([
+      "document.pdf"
+    ])
   })
-  test("4: should be able to record a video", async ({ page }) => {
-    await executeExample(page, 4)
-    const videoCount = await getVideoCount(page)
-    expect(videoCount).toBe(1)
-    const imageNames = await getFileNames(page)
-    expect(imageNames.length).toBe(1)
-    expect(imageNames[0].endsWith(".webm")).toBe(true)
+  test("4: should be able to record a video", async ({ page, tpPage }) => {
+    await tpPage.executeExample(4)
+    await expect(tpPage.images).toHaveCount(0)
+    await expect(tpPage.videos).toHaveCount(1)
+    await expect(tpPage.fileNames).toHaveText([
+      /.*\.webm/
+    ])
   })
-  test("5: should be able to execute something in the browser context", async ({ page }) => {
-    await executeExample(page, 5)
-    const logStatements = await getConsoleLines(page)
+  test("5: should be able to execute something in the browser context", async ({ page, tpPage }) => {
+    await tpPage.executeExample(5)
+    const logStatements = await tpPage.getConsoleLines()
     expect(logStatements.length).toBe(1)
     const parsed = JSON.parse(logStatements[0])
     expect(Object.keys(parsed)).toEqual(["width", "height", "deviceScaleFactor"])
   })
-  test("6: should be able to intercept network requests", async ({ page }) => {
-    await executeExample(page, 6)
-    const logStatements = await getConsoleLines(page)
+  test("6: should be able to intercept network requests", async ({ tpPage }) => {
+    await tpPage.executeExample(6)
+    const logStatements = await tpPage.getConsoleLines()
     expect(logStatements.length).toBeGreaterThan(20) // just so we know that something is going on here
     const allStartsWithHttpOrHttpsProtocol = logStatements.every(entry => entry.startsWith("http://") || entry.startsWith("https://"))
     expect(allStartsWithHttpOrHttpsProtocol).toBe(true)
   })
-  test("7: should be able to intercept and modify network requests", async ({ page }) => {
-    await executeExample(page, 7)
-    const imageCount = await getImageCount(page)
-    expect(imageCount).toBe(1)
-    const imageNames = await getFileNames(page)
-    expect(imageNames).toEqual(["window.png"])
+  test("7: should be able to intercept and modify network requests", async ({ tpPage }) => {
+    await tpPage.executeExample(7)
+    await expect(tpPage.images).toHaveCount(1)
+    await expect(tpPage.fileNames).toHaveText([
+      "window.png"
+    ])
   })
-  test("8: should be able to run the todomvc.com example", async ({ page }) => {
-    await executeExample(page, 8)
-    const videoCount = await getVideoCount(page)
-    expect(videoCount).toBe(1)
-    const imageNames = await getFileNames(page)
-    expect(imageNames.length).toBe(1)
-    expect(imageNames[0].endsWith(".webm")).toBe(true)
+  test("8: should be able to run the todomvc.com example", async ({ tpPage }) => {
+    await tpPage.executeExample(8)
+    await expect(tpPage.videos).toHaveCount(1)
+    await expect(tpPage.fileNames).toHaveText([
+      /.*\.webm/
+    ])
   })
-  test("9: should be able to run the y-combinator crawling example", async ({ page }) => {
-    await executeExample(page, 9)
-    const imageCount = await getImageCount(page)
-    expect(imageCount).toBe(1)
-    const imageNames = await getFileNames(page)
-    expect(imageNames).toEqual(["Y-Combinator.png"])
-    const logStatements = await getConsoleLines(page)
+  test("9: should be able to run the y-combinator crawling example", async ({ tpPage }) => {
+    await tpPage.executeExample(9)
+    await expect(tpPage.images).toHaveCount(1)
+    await expect(tpPage.fileNames).toHaveText([
+      "Y-Combinator.png"
+    ])
+    const logStatements = await tpPage.getConsoleLines()
     expect(logStatements.length).toBeGreaterThan(20)
   })
 });
 
 test.describe("Share functionality", () => {
   test("should not generate share URL for predefined example", async ({ page }) => {
-    await page.goto('', { waitUntil: "networkidle" });
+    await page.goto('?l=javascript');
     await page.click("text='Share'")
     await page.waitForTimeout(500)
     await expect(page).toHaveURL('/?l=javascript&e=page-screenshot')
   })
   test("should generate share URL", async ({ page, baseURL }) => {
-    await page.goto('', { waitUntil: "networkidle" });
+    await page.goto('?l=javascript');
 
     await page.click(".monaco-editor")
     await page.keyboard.press("Meta+KeyA")
@@ -110,9 +120,10 @@ test.describe("Share functionality", () => {
 
     await page.click("text='Share'")
     await page.waitForTimeout(500)
-    expect(page.url().startsWith(`${baseURL}/?l=javascript&s=`)).toBeTruthy()
+    await expect(page).toHaveURL(/\/\?l=javascript&s=.*/)
 
     await page.reload()
+    await page.waitForTimeout(500);
     await page.click("text='Run'")
     await expect(page.locator("data-testid=right-panel >> text=FolioAssert")).toBeVisible();
   })
@@ -122,7 +133,7 @@ test.describe("should handle platform core related features", () => {
   test("should handle the timeout correctly", async ({ page }) => {
     const CODE = `(async () => {
   await new Promise(resolve => setTimeout(resolve, 70 * 1000))`
-    await page.goto('', { waitUntil: "networkidle" });
+    await page.goto('?l=javascript');
     await page.click(".monaco-editor")
     await page.keyboard.press("Meta+KeyA")
     await page.keyboard.type(CODE)
@@ -136,7 +147,7 @@ test.describe("should handle platform core related features", () => {
     });
   })
   test("should handle uncaughtException correctly", async ({ page }) => {
-    await page.goto('');
+    await page.goto('?l=javascript');
     await page.waitForTimeout(200)
     await page.evaluate(() => {
       // @ts-ignore
@@ -158,7 +169,7 @@ const playwright = require("playwright");
     await expect(page.locator("text=Error: foobar!")).toBeVisible();
   })
   test("should prevent access to the control microservice from inside the worker", async ({ page }) => {
-    await page.goto('');
+    await page.goto('?l=javascript');
     await page.waitForTimeout(200)
     await page.evaluate(() => {
       // @ts-ignore
