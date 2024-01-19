3335ea26f38748a4747843fd4adc0e9a49560bf6
Max Schmitt <max@schmitt.mx>
2024-01-19 13:09:04 +0100

    chore: modernise .NET snippets

--- frontend/src/examples/csharp/device-emulation.cs
+++ frontend/src/examples/csharp/device-emulation.cs
@@ -1,16 +1,11 @@
-using Microsoft.Playwright;
+using System;
 using System.Threading.Tasks;
+using Microsoft.Playwright;
 
-class Program
-{
-    public static async Task Main()
-    {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync();
-        var page = await browser.NewPageAsync();
-        var pixel2 = playwright.Devices["Pixel 2"];
-        await using var context = await browser.NewContextAsync(pixel2);
-        await page.GotoAsync("https://playwright.dev/dotnet");
-        await page.ScreenshotAsync(new PageScreenshotOptions { Path = "Pixel-2.png" });
-    }
-}
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync();
+var page = await browser.NewPageAsync();
+var pixel2 = playwright.Devices["Pixel 2"];
+await using var context = await browser.NewContextAsync(pixel2);
+await page.GotoAsync("https://playwright.dev/dotnet");
+await page.ScreenshotAsync(new PageScreenshotOptions { Path = "Pixel-2.png" });
