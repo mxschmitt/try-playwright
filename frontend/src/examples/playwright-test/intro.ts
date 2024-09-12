3b2a2b448c150a427a4a9421a024974b9811265f
Max Schmitt <max@schmitt.mx>
2024-09-12 11:55:24 +0200

    better default test

--- frontend/src/examples/playwright-test/intro.ts
+++ frontend/src/examples/playwright-test/intro.ts
@@ -1,7 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  const title = page.locator('.navbar__inner .navbar__title');
-  await expect(title).toHaveText('Playwright');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
